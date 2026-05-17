851c7e4a567e527b8767f170f6923d4da7017ea8
xkwiatkowski <maksskwiatkowskii@gmail.com>
2026-05-17 14:21:28 +0200

    feat: Randomized Kruskal algorithm

---
+++ src/kruskal.py
@@ -0,0 +1,21 @@
+import random
+from grid import Grid
+from union_find import UnionFind
+
+def kruskal(grid: Grid) -> list[tuple]:
+    """Implements Randomized Kruskal's algorithm. Returns a list of steps taken, where each step is a tuple of the edge and whether it was accepted or not."""
+    edges = grid.edges()
+    random.shuffle(edges)
+
+    uf = UnionFind(list(grid.cells()))
+    steps = []
+
+    for edge in edges:
+        accepted = uf.union(edge.cell_a, edge.cell_b)
+
+        if accepted:
+            grid.add_passage(edge)
+        
+        steps.append((edge, accepted))
+
+    return steps
